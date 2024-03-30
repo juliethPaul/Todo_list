90f00a84903eced68903b01cb6c2646f1282a3c6
juliethpaul <juliethshayo63@gmail.com>
2024-03-30 18:36:26 +0300

    complete task

---
+++ readMe.md
@@ -0,0 +1,3 @@
+# Basic Todo List application using Django
+
+The application allow users to add and delete tasks as well as mark tasks as completed.
---
+++ todoapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/index.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        Task.objects.create(title=title)
+    return redirect('index')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.delete()
+    return redirect('index')
+
+def complete_task(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.completed = True
+    task.save()
+    return redirect('index')
